732c210a18e46e55fefbcbcb00f32d995aede2eb
Albeforia <cnntcyj@126.com>
2023-07-28 18:38:56 +0800

    Implement auto captioning

--- library/preprocess_images_gui.py
+++ library/preprocess_images_gui.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 import gradio as gr
 
+import library.train_util as train_util
 from library.custom_logging import setup_logging
 
 # Set up logging
@@ -31,6 +32,27 @@ def on_images_uploaded(files):
     return [
         output_folder,
         gr.update(value=f"`Images uploaded to: {output_folder}`")
+    ]
+
+
+def clear_upload_images(input_folder):
+    if input_folder == '':
+        return [
+            '',
+            gr.update(value=None),
+            gr.update(value=f"`Nothing to clear`")
+        ]
+
+    dir_path = Path(input_folder)
+    for item in dir_path.iterdir():
+        if item.is_dir():
+            shutil.rmtree(item)
+        else:
+            item.unlink()
+    return [
+        '',
+        gr.update(value=None),
+        gr.update(value=f"`Upload folder cleared`")
     ]
 
 
@@ -135,19 +157,194 @@ def process_images(
         return output()
 
 
+def _add_pre_postfix(
+        folder: str = '',
+        prefix: str = '',
+        postfix: str = '',
+        caption_file_ext: str = '.caption',
+        recursive: bool = False
+) -> None:
+    """
+    Add prefix and/or postfix to the content of caption files within a folder.
+    If no caption files are found, create one with the requested prefix and/or postfix.
+
+    Args:
+        folder (str): Path to the folder containing caption files.
+        prefix (str, optional): Prefix to add to the content of the caption files.
+        postfix (str, optional): Postfix to add to the content of the caption files.
+        caption_file_ext (str, optional): Extension of the caption files.
+    """
+
+    if prefix == '' and postfix == '':
+        return
+
+    # image_extensions = ('.jpg', '.jpeg', '.png', '.webp')
+    # image_files = [
+    #     f for f in os.listdir(folder) if f.lower().endswith(image_extensions)
+    # ]
+    image_files = train_util.glob_images_pathlib(Path(folder), recursive)
+
+    for image_file in image_files:
+        caption_file_name = os.path.splitext(image_file)[0] + caption_file_ext
+        caption_file_path = caption_file_name
+
+        if not os.path.exists(caption_file_path):
+            with open(caption_file_path, 'w', encoding='utf8') as f:
+                separator = ', ' if prefix and postfix else ''
+                f.write(f'{prefix}{separator}{postfix}')
+        else:
+            with open(caption_file_path, 'r+', encoding='utf8') as f:
+                content = f.read()
+                content = content.rstrip()
+                f.seek(0, 0)
+
+                prefix_separator = ', ' if prefix else ''
+                postfix_separator = ', ' if postfix else ''
+                f.write(
+                    f'{prefix}{prefix_separator}{content}{postfix_separator}{postfix}'
+                )
+
+
+def caption_images(
+        train_data_dir,
+        general_threshold,
+        character_threshold,
+        model,
+        undesired_tags,
+        prefix,
+        postfix,
+):
+    # Check for images_dir_input
+    if train_data_dir == '':
+        return gr.update(value=f"`No training data!`")
+
+    log.info(f'Captioning files in {train_data_dir}...')
+    run_cmd = f'accelerate launch "./finetune/tag_images_by_wd14_tagger.py"'
+    run_cmd += f' --batch_size=8'
+    run_cmd += f' --general_threshold={general_threshold}'
+    run_cmd += f' --character_threshold={character_threshold}'
+    run_cmd += f' --caption_extension=".txt"'
+    run_cmd += f' --model="{model}"'
+    run_cmd += (
+        f' --max_data_loader_n_workers=2'
+    )
+
+    run_cmd += f' --recursive'
+    run_cmd += f' --remove_underscore'
+
+    if not undesired_tags == '':
+        run_cmd += f' --undesired_tags="{undesired_tags}"'
+    run_cmd += f' "{train_data_dir}"'
+
+    log.info(run_cmd)
+
+    # Run the command
+    if os.name == 'posix':
+        os.system(run_cmd)
+    else:
+        subprocess.run(run_cmd)
+
+    # Add prefix and postfix
+    _add_pre_postfix(
+        folder=train_data_dir,
+        caption_file_ext='.txt',
+        prefix=prefix,
+        postfix=postfix,
+        recursive=True
+    )
+
+    return gr.update(value='`Captioning done`')
+
+
+# ref: gradio_wd14_caption_gui_tab
+def _gradio_wd14_caption_gui(train_folder, info_text):
+    with gr.Accordion('[2] Captioning', open=False):
+        with gr.Row():
+            prefix = gr.Textbox(
+                label='Prefix to add to WD14 caption',
+                placeholder='(触发词)',
+                interactive=True,
+            )
+
+            postfix = gr.Textbox(
+                label='Postfix to add to WD14 caption',
+                placeholder='(Optional)',
+                interactive=True,
+            )
+
+        undesired_tags = gr.Textbox(
+            label='Undesired tags',
+            placeholder='(Optional) Separate `undesired_tags` with comma `(,)` if you want to remove multiple tags, e.g. `1girl,solo,smile`.',
+            interactive=True,
+        )
+
+        # Model Settings
+        with gr.Row():
+            model = gr.Dropdown(
+                label='Model',
+                choices=[
+                    'SmilingWolf/wd-v1-4-convnext-tagger-v2',
+                    'SmilingWolf/wd-v1-4-convnextv2-tagger-v2',
+                    'SmilingWolf/wd-v1-4-vit-tagger-v2',
+                    'SmilingWolf/wd-v1-4-swinv2-tagger-v2',
+                ],
+                value='SmilingWolf/wd-v1-4-convnextv2-tagger-v2',
+            )
+
+            general_threshold = gr.Slider(
+                value=0.35,
+                label='General threshold',
+                info='Adjust `general_threshold` for pruning tags (less tags, less flexible)',
+                minimum=0,
+                maximum=1,
+                step=0.05,
+            )
+            character_threshold = gr.Slider(
+                value=0.35,
+                label='Character threshold',
+                info='useful if you want to train with character',
+                minimum=0,
+                maximum=1,
+                step=0.05,
+            )
+
+        caption_button = gr.Button('Caption images', variant='primary')
+
+        caption_button.click(
+            caption_images,
+            inputs=[
+                train_folder,
+                general_threshold,
+                character_threshold,
+                model,
+                undesired_tags,
+                prefix,
+                postfix,
+            ],
+            outputs=[info_text],
+            show_progress=False,
+        )
+
+
 def gradio_preprocess_images_gui_tab(headless=False):
     with gr.Tab('数字分身'):
         upload_folder = gr.State('')
         train_folder = gr.State('')
 
-        with gr.Accordion('Select the image folder to upload'):
-            upload_images = gr.File(
-                show_label=False,
-                file_count='directory',
-                # file_types=['image']
-            )
+        with gr.Accordion('Info'):
+            info_text = gr.Markdown()
 
-        with gr.Accordion('Face detection and cropping', open=False):
+        with gr.Accordion('[0] Select the image folder to upload'):
+            with gr.Row().style(equal_height=False):
+                upload_images = gr.File(
+                    show_label=False,
+                    file_count='directory',
+                    # file_types=['image']
+                )
+                clear_upload_button = gr.Button('Clear uploaded images', variant='stop', scale=0)
+                clear_train_button = gr.Button('Clear training folder', variant='stop', scale=0)
+
+        with gr.Accordion('[1] Face detection and cropping', open=False):
             with gr.Row():
                 face_type = gr.Dropdown(
                     label='Face detection type',
@@ -171,14 +368,12 @@ def gradio_preprocess_images_gui_tab(headless=False):
             images_preview = gr.Gallery(preview=True, columns=8)
 
             with gr.Row():
-                submit_images_button = gr.Button('Submit', variant='primary')
-                clear_images_button = gr.Button('Clear', variant='stop')
+                submit_images_button = gr.Button('Process images', variant='primary')
 
-        with gr.Accordion('Train'):
-            pass
+        _gradio_wd14_caption_gui(train_folder, info_text)
 
-        with gr.Accordion('Info'):
-            info_text = gr.Markdown()
+        with gr.Accordion('[3] Train'):
+            pass  # TODO
 
         # Event listeners
         upload_images.upload(
@@ -203,10 +398,19 @@ def gradio_preprocess_images_gui_tab(headless=False):
                 info_text,
             ],
             show_progress=False,
-            api_name='preprocess_images'
         )
 
-        clear_images_button.click(
+        clear_upload_button.click(
+            clear_upload_images,
+            inputs=[upload_folder],
+            outputs=[
+                upload_folder,
+                upload_images,
+                info_text,
+            ],
+            show_progress=False
+        )
+        clear_train_button.click(
             clear_images,
             inputs=[train_folder],
             outputs=[
