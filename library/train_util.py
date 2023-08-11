b9d03db8d689254717214889eea4f3b68ad5f258
Albeforia <cnntcyj@126.com>
2023-08-11 15:21:17 +0800

    Add cache for tokenizer (so we can train offline)

--- library/train_util.py
+++ library/train_util.py
@@ -3514,7 +3514,7 @@ def load_tokenizer(args: argparse.Namespace):
         if args.v2:
             tokenizer = CLIPTokenizer.from_pretrained(original_path, subfolder="tokenizer")
         else:
-            tokenizer = CLIPTokenizer.from_pretrained(original_path)
+            tokenizer = CLIPTokenizer.from_pretrained(original_path, cache_dir='_workspace/tokenizer_cache', _commit_hash='main')
 
     if hasattr(args, "max_token_length") and args.max_token_length is not None:
         print(f"update token length: {args.max_token_length}")
