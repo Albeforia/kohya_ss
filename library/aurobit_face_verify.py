65b3ad9eb5583cb13f55bedc8ba7208425df92c5
Albeforia <cnntcyj@126.com>
2023-09-04 20:09:00 +0800

    Change face verify confidence to 0.9

--- library/aurobit_face_verify.py
+++ library/aurobit_face_verify.py
@@ -146,7 +146,7 @@ async def verify_face_api(input_file):
             start_time = timeit.default_timer()
             _convert_webp_to_png(download_folder)
             files = _get_image_file_paths(download_folder)
-            detected_faces = _process_face_obj(detect_faces(files[0], 0.98, face_model))
+            detected_faces = _process_face_obj(detect_faces(files[0], 0.9, face_model))
             end_time = timeit.default_timer()
             log.info(f"Prediction finished in {(end_time - start_time) * 1000:.2f} ms")
 
@@ -165,7 +165,7 @@ async def verify_face_api(input_file):
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
         file_path = sys.argv[1]
-        detected_faces = _process_face_obj(detect_faces(file_path, 0.98, build_model()))
+        detected_faces = _process_face_obj(detect_faces(file_path, 0.9, build_model()))
         print(_judge_faces(detected_faces, file_path, file_path))
     else:
         pass
