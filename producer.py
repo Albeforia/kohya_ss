17bf6afd5106a3993824663ce7cfee550936a66d
Albeforia <cnntcyj@126.com>
2023-10-10 21:19:00 +0800

    Producer is adaptive to GPU

--- producer.py
+++ producer.py
@@ -3,6 +3,7 @@ import json
 import sys
 import time
 
+import pynvml
 import schedule
 import sentry_sdk
 from confluent_kafka import Producer, KafkaException
@@ -63,6 +64,10 @@ def main():
     db = client[DATABASE_NAME]
     collection = db[COLLECTION_NAME]
 
+    #
+    pynvml.nvmlInit()
+    gpu_handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+
     # 写入消息到Kafka
     def write_message(message, doc_id):
         try:
@@ -77,6 +82,12 @@ def main():
 
     # 从MongoDB读取数据，并写入Kafka
     def read_from_mongo_and_write_to_kafka():
+        utilization = pynvml.nvmlDeviceGetUtilizationRates(gpu_handle)
+        print("GPU Compute Utilization: {}%".format(utilization.gpu))
+        if utilization.gpu > 80:
+            print("Skip because of high GPU utilization")
+            return
+
         doc = collection.find_one_and_update(
             {'status': 0, 'taskType': setting['task_type']},
             {'$set': {'status': 1}},
@@ -89,7 +100,7 @@ def main():
                 write_message(task_id, doc['_id'])
 
     # 使用schedule库定时执行任务
-    schedule.every(10).seconds.do(read_from_mongo_and_write_to_kafka)
+    schedule.every(15).minutes.do(read_from_mongo_and_write_to_kafka)
 
     while True:
         schedule.run_pending()
