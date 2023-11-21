f791f01d52959edf8c8807f98b96a0cb52e109e4
Albeforia <cnntcyj@126.com>
2023-11-21 16:53:57 +0800

    Cascaded face detection

--- library/aurobit_face_verify.py
+++ library/aurobit_face_verify.py
@@ -6,8 +6,10 @@ import random
 import sys
 import timeit
 
+import cv2
 import tensorflow as tf
 from PIL import Image
+from deepface.detectors import FaceDetector
 from retinaface.RetinaFace import build_model, detect_faces
 
 from library.aurobit_utils import download_image
@@ -74,14 +76,12 @@ def _process_face_obj(obj):
     return resp
 
 
-def _judge_faces(detected_faces, local_file, input_file):
-    img = Image.open(local_file)
-
+def _judge_faces(detected_faces, input_img, input_file):
     ratios = []
     for face in detected_faces:
         fw = face[0][2]
         fh = face[0][3]
-        ratio = math.sqrt((fw * fh) / (img.size[0] * img.size[1]))
+        ratio = math.sqrt((fw * fh) / (input_img.shape[0] * input_img.shape[1]))
         ratios.append(ratio)
 
     print(ratios)
@@ -117,6 +117,7 @@ def _judge_faces(detected_faces, local_file, input_file):
 
 def _download_and_detect(input_file, obj_store_setting):
     global face_model
+    global opencv_detector
 
     current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + str(random.randint(0, 1000))
     download_folder = os.path.join('_workspace', 'verify', current_time)
@@ -134,15 +135,30 @@ def _download_and_detect(input_file, obj_store_setting):
     start_time = timeit.default_timer()
     _convert_webp_to_png(download_folder)
     files = _get_image_file_paths(download_folder)
-    detected_faces = _process_face_obj(detect_faces(files[0], 0.9, face_model))
-    end_time = timeit.default_timer()
-    log.info(f"Prediction finished in {(end_time - start_time) * 1000:.2f} ms")
-    return _judge_faces(detected_faces, files[0], input_file)
+    img = cv2.imread(files[0])
+
+    # First detect via OpenCV
+    opencv_face_objs = FaceDetector.detect_faces(opencv_detector, 'opencv', img, False)
+    if len(opencv_face_objs) == 0:
+        # Detect using retinaface if OpenCV failed
+        log.info("Using retinaface")
+        detected_faces = _process_face_obj(detect_faces(img, 0.9, face_model))
+        end_time = timeit.default_timer()
+        log.info(f"Prediction finished in {(end_time - start_time) * 1000:.2f} ms")
+        return _judge_faces(detected_faces, img, input_file)
+    else:
+        opencv_results = []
+        for _, img_region, confidence in opencv_face_objs:
+            opencv_results.append((img_region, confidence))
+        end_time = timeit.default_timer()
+        log.info(f"Prediction finished in {(end_time - start_time) * 1000:.2f} ms")
+        return _judge_faces(opencv_results, img, input_file)
 
 
 async def verify_face_api(input_file):
     global tf_init
     global face_model
+    global opencv_detector
     global is_verifying
 
     if not tf_init:
@@ -151,6 +167,7 @@ async def verify_face_api(input_file):
 
     if 'face_model' not in globals():
         face_model = build_model()
+        opencv_detector = FaceDetector.build_model('opencv')
 
     # while is_verifying:
     #     log.info('Waiting other verify tasks to finish...')
@@ -180,6 +197,6 @@ if __name__ == "__main__":
     if len(sys.argv) >= 2:
         file_path = sys.argv[1]
         detected_faces = _process_face_obj(detect_faces(file_path, 0.9, build_model()))
-        print(_judge_faces(detected_faces, file_path, file_path))
+        print(_judge_faces(detected_faces, cv2.imread(file_path), file_path))
     else:
         pass
